f1714175ac122cf29655f99c15945fa4be2420e3
dmitrivanich <dmitrivanich@gmail.com>
2023-03-17 03:51:24 +0300

    [add] drawer component

---
+++ src/components/DrawerComponent.tsx
@@ -0,0 +1,89 @@
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure,
+  Button,
+  Input
+} from '@chakra-ui/react'
+import { useRef } from 'react'
+import { useTodosStore } from 'store';
+import { TodoForm } from './Todo';
+import { TodoFormOptions } from './Todo/TodoForm';
+import { useCallback } from 'react'
+import AlertComponent from "components/AlertComponent"
+
+export interface DrawerOptions {
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+}
+
+export default function DrawerComponent() {
+  const alertState = useDisclosure()
+  const isEtidTodo = useTodosStore(state => state.isEditTodo)
+  const setIsEtidTodo = useTodosStore(state => state.setIsEditTodo)
+  const selectTodo = useTodosStore(state => state.selectTodo)
+  const selectedTodo = useTodosStore(state => state.selectedTodo)
+  const editTodo = useTodosStore(state => state.editTodo)
+
+  console.log(alertState.isOpen);
+  
+
+  const TodoFormOptions: TodoFormOptions | undefined = selectedTodo ? {
+    todoInfo: selectedTodo,
+    submitBtnName: "Confirm changes",
+    textAreaHeight: "50vh",
+    sumbitBtnAction: (todo) =>{
+      editTodo(todo)
+      setIsEtidTodo(false)
+      selectTodo(null)
+    }
+  } : undefined
+
+  const alertOptions = {
+    header: "Discard changes",
+    message: "Are you sure? You can't undo this action afterwards.",
+    confirmBtnText: "Discard",
+    discardBtnText: "Cancel",
+    
+    confirmAction: () => {
+      setIsEtidTodo(false)
+      selectTodo(null)
+      alertState.onClose()
+    },
+
+    discardAction: () => {
+      alertState.onClose()
+    }
+  }
+
+  return (
+    <>
+      <Drawer
+        isOpen={isEtidTodo}
+        placement='right'
+        onClose={() => alertState.onOpen()}
+      >
+        <DrawerOverlay />
+          <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerHeader>Edit todo</DrawerHeader>
+
+            <DrawerBody>
+              <TodoForm options={TodoFormOptions}/>
+
+              <AlertComponent
+                options={alertOptions} 
+                state={alertState}
+              />
+            </DrawerBody>
+          </DrawerContent>
+      </Drawer>
+    </>
+  )
+}
