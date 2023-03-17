0916314b229b4ea5182a96e2eacc2ed6c00439a9
dmitrivanich <dmitrivanich@gmail.com>
2023-03-17 03:50:31 +0300

    [add] select and edit todo actions

--- src/store.ts
+++ src/store.ts
@@ -8,9 +8,31 @@ export const useTodosStore = create<TodosState>()(
   devtools(
     persist((set,get) => ({
       todos: [],
+      selectedTodo: null,
+      isEditTodo: false,
+
       addTodo: (newTodo) => {set({todos: [...get().todos, newTodo]})},
-      removeTodo: () => {},
-      editTodo: () => {}
+
+      setIsEditTodo: (isEdit) => {
+        set({isEditTodo: isEdit})
+      },
+
+      deleteTodo: (todo) => {
+        let filtredTodos = get().todos.filter(({id}) => id !== todo.id)
+        set({todos: filtredTodos})
+      },
+
+      selectTodo: (todo) => {
+        set({selectedTodo: todo})
+      },
+      
+      editTodo: (newTodo) => {
+        let newTodos = get().todos.map((todo:Todo) => {
+          return todo.id === newTodo.id ? newTodo : todo 
+        })
+
+        set({todos: newTodos})
+      }
     }),
     {
       name: "todos-storage" // name of the key, state will be saved under items
